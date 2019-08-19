7b92d047654c0c1b0e2767f3457446e3067b7390
dendy1 <dendy1@users.noreply.github.com>
2019-08-19 12:26:08 +0300

    Table fixed

---
+++ Assets/Scripts/AirAd/CursorAnimationScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CursorAnimationScript : MonoBehaviour
+{
+    private Vector3 _startPos;
+    private Vector3 _targetPos;
+
+    private Image _content;
+
+    private Color _targetColor;
+    private Color _startColor;
+
+    private float _timer;
+    [SerializeField] private float timeToDisable = 3f;
+
+    void Awake()
+    {
+        _content = gameObject.GetComponent<Image>();
+        _startPos = gameObject.GetComponent<Image>().rectTransform.localPosition;
+        _targetPos = _startPos + new Vector3(0, 150, 0);
+        
+        var color = _content.color;
+        _startColor = color;
+        _targetColor = new Color(color.r, color.g, color.b, 0.1f);
+    }
+
+    private void OnEnable()
+    {
+        _timer = 0;
+        StartCoroutine(Animation());
+    }
+
+    private IEnumerator Animation()
+    {
+        while (gameObject.activeSelf && _timer < timeToDisable)
+        {
+            _content.color = _startColor;
+            _content.rectTransform.localPosition = _startPos;
+            yield return new WaitForSeconds(1f);
+            _targetPos = _startPos;
+            yield return new WaitForSeconds(.1f);
+            _targetPos = _startPos + new Vector3(0, 150, 0);
+        }
+
+        gameObject.SetActive(false);
+    }
+    
+    private void Update()
+    {
+        _content.rectTransform.localPosition = Vector3.Lerp(_content.rectTransform.localPosition, _targetPos, Time.deltaTime * 1.4f);
+        _content.color = Color.Lerp(_content.color, _targetColor, Time.deltaTime * 1.5f);
+
+        _timer += Time.deltaTime;
+    }
+}
--- Assets/Scripts/SingleObjectTrackingController.cs
+++ Assets/Scripts/SingleObjectTrackingController.cs
@@ -1,16 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
 using UnityEngine.UI;
 using Wikitude;
 
 public class SingleObjectTrackingController : SampleController
 {
-    [SerializeField] private ObjectTracker tracker;
-    
     [SerializeField] private Text informationText;
     [SerializeField] private Image informationBackground;
 
     [SerializeField] private GameObject instructionsGameObject;
-    [SerializeField] private GameObject stopExtendedTrackingButton;
 
     public void OnExtendedTrackingQualityChanged(RecognizedTarget target, ExtendedTrackingQuality oldQuality, ExtendedTrackingQuality newQuality)
     {
@@ -34,7 +33,6 @@ public class SingleObjectTrackingController : SampleController
     public void OnTargetRecognized(RecognizedTarget target)
     {
         instructionsGameObject.SetActive(false);
-        stopExtendedTrackingButton.SetActive(true);
         informationText.text = "ObjectTarget: " + target.Drawable.name  + " Recognized";
         informationBackground.color = Color.green;
     }
@@ -43,11 +41,5 @@ public class SingleObjectTrackingController : SampleController
     {
         informationText.text = "ObjectTarget: " + target.Drawable.name + " Lost";
         informationBackground.color = Color.red;
-    }
-    
-    public void OnStopExtendedTrackingButtonPressed() {
-        tracker.StopExtendedTracking();
-        stopExtendedTrackingButton.SetActive(false);
-        informationText.text = "";
     }
 }
