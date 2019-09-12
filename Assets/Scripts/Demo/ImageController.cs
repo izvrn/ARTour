fd102f9c6243ff18f7b3f89524ae27ba9fb94a4e
dendy1 <dendy1@users.noreply.github.com>
2019-09-12 20:53:34 +0300

    hz

--- Assets/Scripts/Demo/ImageController.cs
+++ Assets/Scripts/Demo/ImageController.cs
@@ -14,6 +14,8 @@ public class ImageController : MonoBehaviour
     private Vector3 _localScale;
 
     private Transform _child1, _child2;
+
+    private WikitudeCamera _wikitudeCamera;
     
     private void Start()
     {
@@ -23,6 +25,8 @@ public class ImageController : MonoBehaviour
         _child2 = imageGutterCanvasGameObject.transform.GetChild(1);
         
         _localScale = _child1.localScale;
+
+        _wikitudeCamera = FindObjectOfType<WikitudeCamera>();
     }
     
     private void OnVirtualButtonClicked()
