a4a80c09895cd04a3e66a7850ac3a0485b6bf1fc
dendy1 <dendy1@users.noreply.github.com>
2019-09-29 19:29:42 +0300

    Rotation arrow

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -1,11 +1,16 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class ArrowController : MonoBehaviour
 {
-    public Transform lookAtObject;
+    [SerializeField] private Transform target;
+    [SerializeField] private float rotationSpeed = 1f;
 
-    private void Start()
+    private void Update()
     {
-        transform.LookAt(lookAtObject);
+        Vector3 direction = target.transform.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(direction);
+        Vector3 newRotationVector = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed).eulerAngles;
+        transform.rotation = Quaternion.Euler(0, newRotationVector.y, 0);
     }
 }
