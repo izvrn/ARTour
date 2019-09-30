d3943c4458edfda14866ad59d942c493dccb33d1
dendy1 <dendy1@users.noreply.github.com>
2019-09-30 18:57:19 +0300

    Arrow made

--- Assets/Scripts/ArrowController.cs
+++ Assets/Scripts/ArrowController.cs
@@ -11,6 +11,10 @@ public class ArrowController : MonoBehaviour
         Vector3 direction = target.transform.position - transform.position;
         Quaternion lookRotation = Quaternion.LookRotation(direction);
         Vector3 newRotationVector = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed).eulerAngles;
-        transform.rotation = Quaternion.Euler(0, newRotationVector.y, 0);
+        transform.rotation = Quaternion.Euler(newRotationVector);
+        
+        Debug.DrawRay(transform.position, transform.up, Color.green);
+        Debug.DrawRay(transform.position, transform.forward, Color.blue);
+        Debug.DrawRay(transform.position, transform.right, Color.red);
     }
 }
