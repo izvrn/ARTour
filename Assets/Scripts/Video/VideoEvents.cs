5b5cff09556e91f98ded8d8f5fdca8e351b4b089
dendy1 <borodin_a_o@sc.vsu.ru>
2019-10-03 19:28:37 +0300

    video added

---
+++ Assets/Scripts/Video/VideoEvents.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoEvents : MonoBehaviour
+{
+    private VideoPlayer _vPlayer;
+
+    void Start()
+    {        
+        _vPlayer = GetComponent<VideoPlayer>();
+        _vPlayer.waitForFirstFrame = true;
+    }
+   
+    public void OnTargetRecognized()
+    {
+        _vPlayer.Play();
+    }
+
+    public void OnTargetLost()
+    {
+        _vPlayer.Pause();
+    }
+}
