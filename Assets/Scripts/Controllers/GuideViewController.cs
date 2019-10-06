8197cb61ad26e077a4a6a2afdd638b706f2e3996
dendy1 <borodin_a_o@sc.vsu.ru>
2019-10-06 11:42:38 +0300

    guide views

---
+++ Assets/Scripts/Controllers/GuideViewController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GuideViewController : MonoBehaviour
+{
+    [SerializeField] private Image guideViewImage;
+
+    public void OnTargetReco()
+    {
+        guideViewImage.gameObject.SetActive(false);
+    }
+
+    public void OnTargetLost()
+    {
+        guideViewImage.gameObject.SetActive(true);
+    }
+}
