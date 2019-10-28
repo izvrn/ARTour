470702d99d8352ee12aa41fc627289ceb8825886
dendy1 <dendy1@users.noreply.github.com>
2019-10-28 14:37:54 +0300

    plan tracking fixes

--- Assets/Pump/Scripts/AnimationScript.cs
+++ Assets/Pump/Scripts/AnimationScript.cs
@@ -32,7 +32,7 @@ public class AnimationScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0 && _timer > 1f)
+        if ((Input.touchCount > 0 || Input.GetKeyDown(KeyCode.Mouse0)) && _timer > 1f)
         {
             _timer = 0;
             SwitchWaypoint();
